0024410b0d8b357dfcfa4c8e6af442eaf64c4e87
Thomas Ngueko <ngueokothomas@gmail.com>
2021-07-02 05:34:51 +0400

    KATA: Create rewarding action to reward a quantity

---
+++ src/main/java/actions/reward/RewardQuantityForQuantityAction.java
@@ -0,0 +1,21 @@
+package actions.reward;
+
+import actions.Action;
+import domain.Order;
+/**
+ * Reward a quantity for specific quantity. Ex. TotalQuantity=3 : reward 1 for 2 => TotalQuantity=4
+ * */
+public class RewardQuantityForQuantityAction implements Action<Order> {
+    private final int rewardingQuantity;
+    private final int magicQuantity;
+
+    public RewardQuantityForQuantityAction(int rewardingQuantity, int magicQuantity) {
+        this.rewardingQuantity = rewardingQuantity;
+        this.magicQuantity = magicQuantity;
+    }
+
+    @Override
+    public void execute(Order candidte) {
+
+    }
+}
